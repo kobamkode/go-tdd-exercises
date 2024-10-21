88ed2d56eea429c0edefd7b39849bf75d8562bc8
Mario Wibisono <kobamkode@gmail.com>
2024-10-21 21:22:19 +0700

    wip

---
+++ http-server/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func GetPlayerScore(player string) string {
+	if player == "Pepper" {
+		return "20"
+	}
+
+	if player == "Floyd" {
+		return "10"
+	}
+	return ""
+}
+
+func main() {
+	server := &PlayerServer{}
+	log.Fatal(http.ListenAndServe(":5000", server))
+}
